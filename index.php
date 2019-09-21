38487a276cae76f864ee24eaa64d761680e271db
Wellington Carvalho <wellington59122@gmail.com>
2019-09-21 10:57:00 -0300

    Admin - Categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -122,6 +123,8 @@ $app->post('/admin/users/create', function(){
 
 	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
 
+	$_POST['despassword'] = User::getPasswordHash($_POST['despassword']);
+
 	$user->setData($_POST);
 
 	$user->save();
@@ -131,7 +134,7 @@ $app->post('/admin/users/create', function(){
 
 });
 
-$app->post('/admin/users/:iduser', function($iduser){
+$app->post("/admin/users/:iduser", function($iduser) {
 
 	User::verifyLogin();
 
@@ -143,14 +146,161 @@ $app->post('/admin/users/:iduser', function($iduser){
 
 	$user->setData($_POST);
 
-	$user->update();
-
+	$user->update();	
+	
 	header("Location: /admin/users");
+	exit;
+});
+
+$app->get("/admin/forgot", function() {
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("forgot");	
+});
+
+$app->post("/admin/forgot", function(){
+	
+	$user = User::getForgot($_POST["email"]);
+	
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+
+$app->get("/admin/forgot/sent", function(){
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");	
+});
+
+
+$app->get("/admin/forgot/reset", function(){ 
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+});
+
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = User::getPasswordHash($_POST["password"]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+});
+
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		"categories"=>$categories
+	]);
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
 	exit;
 
 });
 
+$app->get("/admin/categories/:idcategory", function($idcategory){
 
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);	
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();	
+
+	header('Location: /admin/categories');
+	exit;
+
+});
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -2,49 +2,39 @@
 
 namespace Hcode;
 
-class Model{
+class Model {
 
 	private $values = [];
 
 	public function __call($name, $args)
 	{
 		$method = substr($name, 0, 3);
+
 		$fieldName = substr($name, 3, strlen($name));
 
-		switch($method)
+		switch ($method)
 		{
-
 			case "get":
-				return $this->values[$fieldName];
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
 			break;
 
 			case "set":
-				return $this->values[$fieldName] = $args[0];
+				$this->values[$fieldName] = $args[0];
 			break;
-
 		}
-
-
 	}
 
 	public function setData($data = array())
 	{
-
 		foreach ($data as $key => $value) {
 			
 			$this->{"set".$key}($value);
-
 		}
-
 	}
-
 	public function getValues()
 	{
-
 		return $this->values;
-
 	}
-
 }
 
- ?>
+?>
